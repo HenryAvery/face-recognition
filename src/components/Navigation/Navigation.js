cce08848e51a94a3eeb8d82eb98fa2bc555ff198
HenryAvery <HenryAveryDev@gmail.com>
2019-03-06 17:14:00 -0600

    remove boilerplate add logo/nav component add background styles

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav class='flex justify-end'>
+            <p className='f3 link dim underline pa3 pointer'>Sign out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
