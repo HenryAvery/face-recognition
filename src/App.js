fa7961d31467823fad68ca59aa5558fee716d9c6
HenryAvery <HenryAveryDev@gmail.com>
2019-03-06 18:20:06 -0600

    add ImageLinkForm and Rank components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import 'tachyons';
 
 class App extends Component {
@@ -10,6 +12,10 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*
+        <FaceRecognition />*/}
       </div>
     );
   }
