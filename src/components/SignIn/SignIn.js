4538b7d2318c347eed0e9b0871a3aacb150baa3f
HenryAvery <HenryAveryDev@gmail.com>
2019-03-29 22:17:13 -0500

    correction spelling error in signin

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SignIn extends React.Component {
     constructor(props){
         super(props);
-        this.stat = {
+        this.state = {
             signInEmail: '',
             signInPassword: '',
         }
